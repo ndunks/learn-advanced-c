79a4cd9277b7f4017b79e704541781f08cc5f0a5
Mochamad Arifin <klampok.child@gmail.com>
2020-07-22 11:05:09 +0700

    warn all

--- 6-pthread/main.c
+++ 6-pthread/main.c
@@ -2,19 +2,20 @@
 #include <time.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <malloc.h>
 
 void *busy_thread(void *param)
 {
     char *retval = malloc(64);
     int len;
     pthread_t me = pthread_self();
-    printf("thread start %x\n", me);
+    printf("thread start %lx\n", me);
 
     len = sprintf(retval, "%lu", time(NULL));
     retval[len] = ' ';
     sleep(5);
     len = sprintf(retval + len + 1, "%lu", time(NULL));
-    printf("thread exit %x\n", me);
+    printf("thread exit %lx\n", me);
     pthread_exit(retval);
 }
 
@@ -22,10 +23,10 @@ int main(int argc, char const *argv[])
 {
     char *retval;
     pthread_t me = pthread_self(), thread;
-    printf("main start %x\n", me);
+    printf("main start %lx\n", me);
 
     pthread_create(&thread, NULL, busy_thread, NULL);
-    pthread_join(thread, &retval);
+    pthread_join(thread, (void *)&retval);
     printf("RET: %s\n", retval);
-    printf("main exit %x\n", me);
+    printf("main exit %lx\n", me);
 }
