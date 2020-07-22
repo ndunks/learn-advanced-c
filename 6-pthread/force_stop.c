0ad5985cd6d11ea37fb33842cea2ccaecd261fd5
Mochamad Arifin <klampok.child@gmail.com>
2020-07-22 12:31:05 +0700

    ok

--- 6-pthread/force_stop.c
+++ 6-pthread/force_stop.c
@@ -2,30 +2,42 @@
 #include <time.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <malloc.h>
+
+char buf[255] = {0};
 
 void *busy_thread(void *param)
 {
     char *retval = malloc(64);
     int len;
     pthread_t me = pthread_self();
-    printf("thread start %x\n", me);
+
+    printf("thread start %lx\n", me);
+    pthread_setcancelstate(PTHREAD_CANCEL_ENABLE, NULL);
+
+    scanf("%255s", buf);
 
     len = sprintf(retval, "%lu", time(NULL));
     retval[len] = ' ';
-    sleep(5);
+    
     len = sprintf(retval + len + 1, "%lu", time(NULL));
-    printf("thread exit %x\n", me);
+    printf("thread exit %lx\n", me);
     pthread_exit(retval);
 }
 
 int main(int argc, char const *argv[])
 {
-    char *retval;
+    char *retval = NULL;
+    int ret;
     pthread_t me = pthread_self(), thread;
-    printf("main start %x\n", me);
+    printf("main start %lx\n", me);
 
     pthread_create(&thread, NULL, busy_thread, NULL);
-    pthread_join(thread, &retval);
-    printf("RET: %s\n", retval);
-    printf("main exit %x\n", me);
+    sleep(5);
+    ret = pthread_cancel(thread);
+    pthread_join(thread, (void *)&retval);
+    printf("canceled %d\n", ret);
+    printf("RET: %p\n", retval);
+    printf("main exit %lx\n", me);
+    printf("Thread read: %s\n", buf);
 }
